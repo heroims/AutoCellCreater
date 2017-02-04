a742793bb4e3f40a894f438f95fff8a75d0bac20
zhaoyiqi <heroims@163.com>
2017-02-04 17:21:13 +0800

    Initial Commit

---
+++ AutoCellCreaterDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoCellCreaterDemo
+//
+//  Created by admin on 2017/2/4.
+//  Copyright © 2017年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
