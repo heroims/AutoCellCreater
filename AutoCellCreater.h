6c9240921caaa343991800f823a6fdc6c77dbbfe
zhaoyiqi <heroims@163.com>
2017-05-08 13:51:32 +0800

    添加 .h头

---
+++ AutoCellCreater.h
@@ -0,0 +1,15 @@
+//
+//  AutoCellCreater.h
+//  AutoCellCreaterDemo
+//
+//  Created by admin on 2017/5/8.
+//  Copyright © 2017年 admin. All rights reserved.
+//
+
+#ifndef AutoCellCreater_h
+#define AutoCellCreater_h
+
+#import "AutoCellCreaterCollectionView.h"
+#import "AutoCellCreaterTableView.h"
+
+#endif /* AutoCellCreater_h */
